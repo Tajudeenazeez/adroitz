f01a0ade55e7351305ef025e576ab94c990edcbb
tajudeenazeez <azeezproto@gmail.com>
2022-12-16 09:12:13 +0100

    program offered added

---
+++ pages/Program.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Flex, Heading } from "@chakra-ui/react";
+import Link from '../components/NavLink'
+interface ProgramProps {
+  text: string;
+}
+
+const Program: React.FC<ProgramProps> = ({ text }) => {
+  return (
+    <Flex
+      h="288px"
+      flexDir="column"
+      alignItems="center"
+      justifyContent="space-evenly"
+      boxShadow="lg"
+      borderRadius={5}
+      p={4}
+    >
+      <Heading lineHeight={1.6} textAlign="center" fontSize="md">
+        {text}
+      </Heading>
+      <Link
+        type="button"
+        pathName="https://docs.google.com/forms/d/e/1FAIpQLSdltDm3s9mSpshCEjhh81TI8ZhriSaXLJmHecauM0xfzdAc5w/viewform?usp=sf_link"
+        isExternal
+        text="Enroll"
+      />
+    </Flex>
+  );
+};
+
+export default Program;
