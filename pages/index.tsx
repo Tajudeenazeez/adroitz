1734cdab5d1d4e386077c29375e4acffdf105509
tajudeenazeez <azeezproto@gmail.com>
2022-12-16 08:06:40 +0100

    headed implemented

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-
+import { Heading } from '@chakra-ui/react'
 
 export default function Home() {
   return (
@@ -10,7 +10,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Welcome to Adroitz coding school for kids</div>
+      <Heading>
+        Welcome to Adroitz coding school for kids
+      </Heading>
     </>
   )
 }
